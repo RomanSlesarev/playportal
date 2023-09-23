c772006303e90b2090e15957625a34520913ba4c
CORP\r.slesarev <r.slesarev@skai.online>
2023-09-23 17:25:34 +0300

    Hello

---
+++ Rjomba/Server/Services/RjombaGamesService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Concurrent;
+
+namespace Rjomba.Server.Services
+{
+    public class RjombaGamesService : BackgroundService
+    {
+        private ConcurrentDictionary<string, DateTime> _games;
+
+        public RjombaGamesService()
+        {
+            _games = new ConcurrentDictionary<string, DateTime>();
+        }
+
+        public void Add(string gameId)
+        {
+            _games.TryAdd(gameId, DateTime.Now);
+        }
+
+        public void Update(string gameId)
+        {
+            _games[gameId] = DateTime.Now;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(5000);
+
+                foreach (var game in _games)
+                {
+                    if (DateTime.Now - game.Value > TimeSpan.FromMinutes(5))
+                    {
+                        _games.TryRemove(game);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Rjomba/Server/Signal/RjombaHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Rjomba.Server.Services;
+
+namespace BlazorWebAssemblySignalRApp.Server.Hubs;
+
+public class RjombaHub : Hub
+{
+    private RjombaGamesService _rjombaService;
+
+    public RjombaHub(RjombaGamesService rjombaService)
+    {
+        _rjombaService = rjombaService;
+    }
+
+    public async Task InitializeGame(string GameId)
+    {
+        _rjombaService.Add(GameId);
+    }
+
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
